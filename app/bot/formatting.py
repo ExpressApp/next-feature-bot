430143593cf66e2300fd6c4bf0c69aad2b9f1952
Kiruha01 <liss-2001g@mail.ru>
2022-05-20 13:07:34 +0300

    Fix pformat_str_json function

--- app/bot/formatting.py
+++ app/bot/formatting.py
@@ -5,7 +5,7 @@ from typing import Any, Dict
 
 
 def pformat_str_json(json_str: str) -> str:
-    return json.dumps(json.loads(json_str), sort_keys=True, indent=4)
+    return pformat_json(json.loads(json_str))
 
 
 def pformat_json(json_dict: Dict[str, Any]) -> str:
